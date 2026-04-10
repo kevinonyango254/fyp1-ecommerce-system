3d1fb642df80c79bd4cfa4ad79dc9358819acc7d
Osborn <admin@gmail.com>
2026-04-10 17:18:02 +0300

    Auto create superadmin for production

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,19 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        from .models import UserProfile
+
+        if not User.objects.filter(username='superadmin').exists():
+            user = User.objects.create_superuser(
+                username='superadmin',
+                email='academicexpert10@gmail.com',
+                password='admin1234'
+            )
+
+            UserProfile.objects.get_or_create(
+                user=user,
+                defaults={'role': 'admin'}
+            )
