755e5a3a8ef8527d0adbc00265193b1646737096
Osborn <admin@gmail.com>
2026-07-23 11:03:27 +0300

    Fix PasswordResetView import

--- accounts/views.py
+++ accounts/views.py
@@ -222,11 +222,5 @@ import traceback
 
 class DebugPasswordResetView(PasswordResetView):
     def form_valid(self, form):
-        try:
-            return super().form_valid(form)
-        except Exception as e:
-            print("=" * 60)
-            print("PASSWORD RESET ERROR")
-            traceback.print_exc()
-            print("=" * 60)
-            raise
+        print("EMAILS FOUND:", list(form.get_users(form.cleaned_data["email"])))
+        return super().form_valid(form)
